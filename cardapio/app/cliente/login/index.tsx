f68adcc7164663e3a45ee67969d8cd050000f7f7
Stefano <133257559+Stefano-8bit@users.noreply.github.com>
2025-05-07 10:56:11 -0300

    adicionado styles no login do cliente

--- cardapio/app/cliente/login/index.tsx
+++ cardapio/app/cliente/login/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { usePedido } from '../../../hooks/usePedido';
 import { useAuth } from '../../../hooks/useAuth';
 import { router } from 'expo-router';
 import { useState } from 'react';
+import { styles } from './login.styles';
 
-export default function Login() {
+export default function LoginCliente() {
   const { setPedido } = usePedido();
   const { login } = useAuth();
 
@@ -19,22 +20,33 @@ export default function Login() {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPgDcx-I2OLjGtFKlaAY1B89BZASmqHcZQ2w&s" alt="imagem logo" width={200} />
-      <Text>Login</Text>
-      <TextInput
-        placeholder="Nome"
-        value={nome}
-        onChangeText={setNome}
-        style={{ marginBottom: 10, borderBottomWidth: 1 }}
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPgDcx-I2OLjGtFKlaAY1B89BZASmqHcZQ2w&s' }}
+        style={styles.banner}
+        resizeMode="cover"
       />
-      <TextInput
-        placeholder="CPF"
-        value={cpf}
-        onChangeText={setCpf}
-        style={{ marginBottom: 20, borderBottomWidth: 1 }}
-      />
-      <Button title="Avançar" onPress={handleLogin} />
+      <View style={styles.form}>
+        <Text style={styles.label}>Nome:</Text>
+        <TextInput
+          placeholder="Digite seu nome"
+          placeholderTextColor="#999"
+          style={styles.input}
+          value={nome}
+          onChangeText={setNome}
+        />
+        <Text style={styles.label}>CPF:</Text>
+        <TextInput
+          placeholder="Digite seu CPF"
+          placeholderTextColor="#999"
+          style={styles.input}
+          value={cpf}
+          onChangeText={setCpf}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Avançar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
